5ef9ef803c9a1a7abf4e8fcb28b3a3f16ec4a18e
Astrid Gealer <astrid@gealer.email>
2024-05-10 10:49:55 +0100

    add databaseTransaction helper

--- packages/arpc-core/src/transactions.ts
+++ packages/arpc-core/src/transactions.ts
@@ -28,3 +28,54 @@ export function rollback(fn: () => Promise<void>) {
     }
     a[1].push(fn);
 }
+
+interface DBTransaction {
+    commit(): any;
+    rollback(): any;
+}
+
+const _dbTxMagicKey = Symbol("arpcDBTx");
+
+// Defines a function to start a database transaction, or fetch the current one if the transaction creation
+// function was passed in before within the current request (either single or within a batch). Note for this,
+// it is important you pass in the same function reference in each call.
+export function databaseTransaction<
+    Tx extends DBTransaction, TxReturn extends Promise<Tx> | Tx,
+>(creator: () => TxReturn): TxReturn {
+    const ctx = workerContext();
+    if (!ctx) {
+        throw new Error("databaseTransaction() was requested outside of a arpc worker context");
+    }
+
+    let m: Map<any, any> = ctx.get(_dbTxMagicKey);
+    if (!m) {
+        m = new Map();
+        ctx.set(_dbTxMagicKey, m);
+    }
+
+    let tx = m.get(creator);
+    if (!tx) {
+        tx = creator();
+        m.set(creator, tx);
+    }
+    if ("then" in tx) {
+        function caller(key: string) {
+            return async () => {
+                let t: any;
+                try {
+                    t = await tx;
+                } catch {
+                    // Would have failed in the call anyway.
+                    return;
+                }
+                await t[key]();
+            };
+        }
+        commit(caller("commit"));
+        rollback(caller("rollback"));
+    } else {
+        commit(tx.commit);
+        rollback(tx.rollback);
+    }
+    return tx;
+}
