dabd13fba53d5677ebc5085b912ab88ea9e52f15
Astrid Gealer <astrid@gealer.email>
2024-05-22 14:16:45 +0100

    Add built in exceptions

--- packages/arpc-schema-viewer/src/builtinExceptions.ts
+++ packages/arpc-schema-viewer/src/builtinExceptions.ts
@@ -1,5 +1,22 @@
 import { Exception } from "@arpc/client-gen";
 
 export const builtinExceptions: Exception[] = [
-    // TODO
+    // InvalidResponse is added in the client generator.
+
+    {
+        name: "BadRequest",
+        description: "Thrown when a request is invalid.",
+    },
+    {
+        name: "Ratelimited",
+        description: "Thrown when a user is ratelimited.",
+    },
+    {
+        name: "Unauthorized",
+        description: "Thrown when a user is not authorized.",
+    },
+    {
+        name: "InternalServerError",
+        description: "Thrown when the server encounters an internal error.",
+    },
 ];
