c7341fd6472d53e2eb9f23b3c963d34cee5220bb
Astrid Gealer <astrid@gealer.email>
2024-06-13 18:43:11 +0100

    Add useRequestBatcher

--- packages/arpc-react-hooks/hooks.ts
+++ packages/arpc-react-hooks/hooks.ts
@@ -1,8 +1,5 @@
 import React from "react";
 
-// Extracts the instance type from a class constructor.
-type ExtractInstance<T> = T extends new (...args: any) => infer R ? R : never;
-
 // Makes sure the input includes a batcher.
 interface IncludesBatcher<BatchFn> {
     batch(fn: BatchFn, signal?: AbortSignal): any;
@@ -12,10 +9,11 @@ interface IncludesBatcher<BatchFn> {
 export class APIClientAtom<
     Args extends any[],
     BatchFn extends (batcher: any) => any,
-    Constructor extends new (...args: Args) => IncludesBatcher<BatchFn>,
+    Instance extends IncludesBatcher<BatchFn>,
+    Constructor extends new (...args: Args) => Instance,
 > {
     private _constructor: Constructor;
-    private _instance: ExtractInstance<Constructor>;
+    private _instance: Instance;
     private _events = new Set<() => void>();
 
     constructor(constructor: Constructor, ...args: Args) {
@@ -47,23 +45,153 @@ export class APIClientAtom<
 }
 
 // Return the return type of the get method.
-type GetReturn<Atom extends APIClientAtom<any, any, any>> = ReturnType<Atom["get"]>;
+type GetReturn<Atom extends APIClientAtom<any, any, any, any>> = ReturnType<Atom["get"]>;
 
 // A low level hook to return the API client and re-render when it changes.
-export function useClientAtomInstance<Atom extends APIClientAtom<any, any, any>>(atom: Atom): GetReturn<Atom> {
+export function useClientAtomInstance<Atom extends APIClientAtom<any, any, any, any>>(atom: Atom): GetReturn<Atom> {
     // @ts-expect-error: This is a private API.
     return atom._use();
 }
 
 // Get the batcher function from a atom.
-type BatcherFunction<T> = T extends APIClientAtom<any, infer BatchFn, any> ? BatchFn : never;
+type BatcherFunction<T> = T extends APIClientAtom<any, infer BatchFn, any, any> ? BatchFn : never;
+
+// Defines the internal type used for promise responses.
+type InternalPromiseResult<T> = [T, undefined] | [undefined, Error];
+
+// Defines a map to store the promise results.
+const promiseResults = new Map<number, InternalPromiseResult<any>>();
+
+// Defines the hook to make an async request.
+function useAsync<T>(fn: () => Promise<T>, deps: any[]) {
+    // Get a ID tied to the lifecycle so we can track the promise. Note we can't use useEffect since otherwise we will be
+    // setting in the render phase.
+    const id = React.useMemo(() => Math.random(), deps);
+
+    // If this is not the first render, we need to check if the promise is already resolved.
+    const res = promiseResults.get(id);
+    if (res) {
+        // Delete it since this only fires once.
+        promiseResults.delete(id);
+
+        // Return the resolved promise.
+        if (res[1]) throw res[1];
+        return res[0] as T;
+    }
+
+    // Throw the promise so React suspends.
+    throw fn().then((result) => {
+        promiseResults.set(id, [result, undefined]);
+    }).catch((error) => {
+        promiseResults.set(id, [undefined, error]);
+    });
+}
+
+// Defines what a internal cache value looks like.
+type InternalCacheValue = [Promise<any>, number];
+
+// Defines the internal cache.
+const cache = new Map<any, Map<string, InternalCacheValue>>();
+
+// Defines a hook to use the cache key or purge it.
+function useCacheKey(client: any) {
+    const cacheKey = React.useRef<string | undefined>();
+    const purgeCacheKey = () => {
+        if (cacheKey.current) {
+            // Get the cache value and check if by removing one the number of references is zero.
+            const clientMap = cache.get(client);
+            if (!clientMap) return;
+            const value = clientMap.get(cacheKey.current);
+            if (value && --value[1] === 0) {
+                // Delete the cache key if there are no references.
+                clientMap.delete(cacheKey.current);
+
+                // If there are no more cache keys, delete the client map.
+                if (clientMap.size === 0) {
+                    cache.delete(client);
+                }
+            }
+        }
+    };
+    React.useEffect(() => purgeCacheKey, [client]);
+    return [cacheKey, purgeCacheKey] as const;
+}
+
+// Defines the options for request based hooks.
+export type RequestOptions = {
+    /**
+     * If set to true, the request will be re-run when the client changes. Defaults to true.
+     */
+    rerun?: boolean;
+
+    /**
+     * Defines the keys that are used to cache the request. Note that if the keys change, the request will be re-run.
+     */
+    cacheKeys?: string[];
+};
 
 // A hook to manage making a batch of API requests and then rerunning them when the client changes (unless turned off).
 export function useRequestBatcher<
-    Atom extends APIClientAtom<any, any, any>,
->(atom: Atom, batcher: BatcherFunction<Atom>, rerun?: boolean) {
+    Atom extends APIClientAtom<any, any, any, any>,
+>(atom: Atom, fn: BatcherFunction<Atom>, options?: RequestOptions) {
     // Call the low level hook to get the client.
     const client = useClientAtomInstance(atom);
 
+    // Defines the abort controller to cancel the batch.
+    const controller = React.useMemo(() => new AbortController(), []);
+    React.useEffect(() => () => controller.abort(), [controller]);
 
+    // Get the cache key and purge function.
+    const [cacheKey, purgeCacheKey] = useCacheKey(client);
+
+    // Get the dependencies for the async hook.
+    const deps: any[] = [options?.cacheKeys, options?.rerun, client];
+    if (options?.rerun === false) {
+        // If rerun is explicitly set to false, we need to pop client since we don't want to re-render.
+        deps.pop();
+    }
+
+    // Call the async hook to make the request.
+    return useAsync(async () => {
+        // Call the batcher function.
+        const batchInit = fn(client);
+        const result = "then" in batchInit ? await batchInit : batchInit;
+
+        // Build the cache key.
+        const thisCacheKey = JSON.stringify([
+            options?.cacheKeys,
+            result,
+        ]);
+        let incr = false;
+        if (cacheKey.current !== thisCacheKey) {
+            // Purge our old cache key.
+            purgeCacheKey();
+
+            // Set the cache key.
+            cacheKey.current = thisCacheKey;
+
+            // If we have a cache value, we should increment the reference count.
+            incr = true;
+        }
+
+        // Check if we have a cache value.
+        const cacheValue = cache.get(client)?.get(thisCacheKey);
+        if (cacheValue) {
+            // Increment the reference count if we haven't already.
+            if (incr) cacheValue[1]++;
+
+            // Return the cache value.
+            return cacheValue[0];
+        }
+
+        // Run the batcher function and return the promise.
+        const p = client.batch(() => result, controller.signal);
+        let clientsMap = cache.get(client);
+        if (!clientsMap) {
+            clientsMap = new Map();
+            cache.set(client, clientsMap);
+        }
+        clientsMap.set(thisCacheKey, [p, 1]);
+        return p;
+    }, deps);
 }
