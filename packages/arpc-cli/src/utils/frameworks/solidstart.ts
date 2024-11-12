d118e23697aab7bcaca2a29f1057e5c61ba42785
Astrid Gealer <astrid@gealer.email>
2024-11-12 17:56:20 +0000

    preliminary solidstart support

--- packages/arpc-cli/src/utils/frameworks/solidstart.ts
+++ packages/arpc-cli/src/utils/frameworks/solidstart.ts
@@ -1,9 +1,28 @@
 import { join } from "path";
 import { readFileSync } from "fs";
+import { mkdir, writeFile } from "fs/promises";
 import type { Framework } from ".";
 
 async function writeEntrypoints(solidFolder: string) {
+    const routesDir = join(solidFolder, "src", "routes");
+    const apiDir = join(routesDir, "api");
+    await mkdir(apiDir, { recursive: true }).then(() => Promise.all([
+        // Write /api/rpc
+        writeFile(
+            join(apiDir, "rpc.ts"),
+            `import type { APIEvent } from "@solidjs/start/server";
+import { httpHandler } from "@/rpc";
 
+function solidWrap({ request }: APIEvent) {
+    return httpHandler(request);
+}
+
+export const GET = solidWrap;
+
+export const POST = solidWrap;
+`,
+        ),
+    ]))
 }
 
 const APP_CONFIG_REGEX = /^app\.config\.[mc]?[tj]sx?$/;
